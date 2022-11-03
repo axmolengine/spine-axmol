48bb224dc639319b457a8b2f8ce7b5ba39b461b9
一线灵|Deal <halx99@live.com>
2022-11-03 15:02:38 +0800

    Fix RotateTimeline

--- runtime/src/spine/RotateTimeline.cpp
+++ runtime/src/spine/RotateTimeline.cpp
@@ -112,7 +112,7 @@ void RotateTimeline::apply(Skeleton &skeleton, float lastTime, float time, Vecto
 			r += bone->_data._rotation - bone->_rotation;
 			// Fall through.
 		case MixBlend_Add:
-			bone->_rotation += (r - (16384 - (int)(16384.499999999996 - r / 360)) * 360) * alpha;
+			bone->_rotation += r * alpha; // don't needs mod degree again
 	}
 }
 
